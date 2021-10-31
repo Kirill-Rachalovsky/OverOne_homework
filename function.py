64e1e99342d7f5b53674a4e646efc2edf72a2edf
Kirill-Rachalovsky <devis5308@gmail.com>
2021-10-31 19:42:26 +0300

    'simple_calc_v2.1'

--- function.py
+++ function.py
@@ -1,22 +1,23 @@
 def simple_calc(x, action, y):
     x = int(x)
     y = int(y)
+    res = None
     if action == '+':
-        res = 'Ответ: ' + str(x + y)
+        res = x + y
     elif action[0] == '-':
-        res = 'Ответ: ' + str(x - y)
+        res = x - y
     elif action[0] == '*':
-        res = 'Ответ: ' + str(x * y)
+        res = x * y
     elif action[0] == '/':
-        res = 'Ответ: ' + str(x / y)
+        res = x / y
     return res
-
 
 
 try:
     a, action, b = input('Введите выражение, через пробелы: ').split()
-    print(simple_calc(a, action, b))
+    print('Ответ: ',  simple_calc(a, action, b))
 except ValueError:
     print('\nВыражение введено некорректно!')
     print('Выражение необходимо вводить через пробелы.')
     print("Доступные действия: '+', '-', '*', '/'.\n")
+
