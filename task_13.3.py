81aa46801b8446554712abd00c39115b5ab3cf17
Kirill-Rachalovsky <devis5308@gmail.com>
2021-11-01 21:54:08 +0300

    'add_task_13.3'

---
+++ task_13.3.py
@@ -0,0 +1,11 @@
+def transformation(lst):
+    for i in lst:
+        if i % 2 == 1:
+            lst.remove(i)
+        else:
+            lst[lst.index(i)] = int(i/2)
+    return lst
+
+print(transformation([852, 85, 784, 58]))
+
+
